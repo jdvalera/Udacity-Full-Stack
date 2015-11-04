883e51daf7ef0d6607ef77da4d8b029a862e6bd6
jdvalera <johnlemuelvalera@yahoo.com>
2015-11-04 09:21:59 -0800

    Revert back to Python 2.7 syntax from Python 3.5 syntax

--- Project2/vagrant/tournament/tournament_test.py
+++ Project2/vagrant/tournament/tournament_test.py
@@ -6,13 +6,13 @@ from tournament import *
 
 def testDeleteMatches():
     deleteMatches()
-    print "1. Old matches can be deleted."
+    print"1. Old matches can be deleted."
 
 
 def testDelete():
     deleteMatches()
     deletePlayers()
-    print "2. Player records can be deleted."
+    print"2. Player records can be deleted."
 
 
 def testCount():
@@ -24,7 +24,7 @@ def testCount():
             "countPlayers() should return numeric zero, not string '0'.")
     if c != 0:
         raise ValueError("After deleting, countPlayers should return zero.")
-    print "3. After deleting, countPlayers() returns zero."
+    print"3. After deleting, countPlayers() returns zero."
 
 
 def testRegister():
