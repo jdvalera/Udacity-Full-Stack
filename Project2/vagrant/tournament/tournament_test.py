3ed396ee25a376b819726023b201716d63019ef6
jdvalera <johnlemuelvalera@yahoo.com>
2015-11-10 15:58:44 -0800

    Change some functions to test added features.

--- Project2/vagrant/tournament/tournament_test.py
+++ Project2/vagrant/tournament/tournament_test.py
@@ -11,6 +11,7 @@ def testDeleteMatches():
 
 def testDelete():
     deleteMatches()
+    deleteRegisteredPlayers()
     deletePlayers()
     print"2. Player records can be deleted."
 
@@ -67,12 +68,12 @@ def testStandingsBeforeMatches():
                          "they have played any matches.")
     elif len(standings) > 2:
         raise ValueError("Only registered players should appear in standings.")
-    if len(standings[0]) != 4:
-        raise ValueError("Each playerStandings row should have four columns.")
-    [(id1, name1, wins1, matches1), (id2, name2, wins2, matches2)] = standings
-    if matches1 != 0 or matches2 != 0 or wins1 != 0 or wins2 != 0:
+    if len(standings[0]) != 9:
+        raise ValueError("Each playerStandings row should have nine columns.")
+    [(t_id1, p_id1, name1, wins1, draws1, matches1, score1, oms1, byes1), (t_id2, p_id2, name2, wins2, draws2, matches2, score2, oms2, byes2)] = standings
+    if matches1 != 0 or matches2 != 0 or wins1 != 0 or wins2 != 0 or draws1 != 0 or draws2 != 0 or score1 != 0 or score2 != 0 or oms1 != None or oms2 != None or byes1 != 0 or byes2 != 0:
         raise ValueError(
-            "Newly registered players should have no matches or wins.")
+            "Newly registered players should have no matches, wins, draws, scores, byes or oms.")
     if set([name1, name2]) != set(["Melpomene Murray", "Randy Schwartz"]):
         raise ValueError("Registered players' names should appear in standings, "
                          "even if they have no matches played.")
@@ -132,8 +133,8 @@ if __name__ == '__main__':
     testRegister()
     testRegisterCountDelete()
     testStandingsBeforeMatches()
-    testReportMatches()
-    testPairings()
+    #testReportMatches()
+    #testPairings()
     print "Success!  All tests pass!"
 
 
