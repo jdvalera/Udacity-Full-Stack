3649bf666461b6519583084d314173c84f461a22
jdvalera <johnlemuelvalera@yahoo.com>
2015-11-11 14:14:41 -0800

    Change tournament.sql to fix bug where if a player is registered to two tournaments there would be duplicates of that player in the standings. Made use of COALESCE(foo.t_id, 0) to give foo.t_id a default value of 0 if selecting it and it is null.

--- Project2/vagrant/tournament/tournament.sql
+++ Project2/vagrant/tournament/tournament.sql
@@ -26,38 +26,41 @@ CREATE TABLE registeredPlayers (t_id INTEGER REFERENCES tournaments (id),
 -- Wins View
 -- Creates a view showing wins for each player
 CREATE VIEW v_wins AS 
-        SELECT foo.t_id, foo.id, foo.name, COUNT(matches.winner) AS wins 
+        SELECT COALESCE(foo.t_id, 0) AS t_id, foo.id, foo.name, COUNT(matches.winner) AS wins 
         FROM (SELECT * FROM players LEFT JOIN registeredPlayers ON players.id = registeredPlayers.p_id) AS foo LEFT JOIN matches 
-    	ON foo.id = matches.winner AND matches.draw != 't' GROUP BY foo.t_id, foo.id, foo.name;
+    	ON foo.id = matches.winner AND matches.draw != 't' AND matches.t_id = foo.t_id GROUP BY foo.t_id, foo.id, foo.name;
 
 
 -- Draws View
 -- Creates a view showing draws for each player
 CREATE VIEW v_draws AS
-    SELECT foo.t_id, foo.id, foo.name, COUNT(matches.draw) AS draws
+    SELECT COALESCE(foo.t_id, 0) AS t_id, foo.id, foo.name, COUNT(matches.draw) AS draws
     FROM (SELECT * FROM players LEFT JOIN registeredPlayers ON players.id = registeredPlayers.p_id) AS foo LEFT JOIN matches
-    ON (foo.id = matches.winner OR foo.id = matches.loser) AND matches.draw = 't' GROUP BY foo.t_id, foo.id, foo.name;
+    ON (foo.id = matches.winner OR foo.id = matches.loser) AND matches.draw = 't' AND matches.t_id = foo.t_id GROUP BY foo.t_id, foo.id, foo.name;
 
 -- Wins/Draws/Score View
 -- Create a view showing wins/draws/score for each player.
 -- Win = 3pts. Loss = 0pts. Draw = 1pt.
 CREATE VIEW v_scores AS
     SELECT v_wins.t_id,v_wins.id,v_wins.name,v_wins.wins,v_draws.draws,(wins*3)+draws AS score 
-    FROM v_wins,v_draws WHERE v_wins.id = v_draws.id ORDER BY t_id, score DESC;
+    FROM v_wins,v_draws WHERE v_wins.id = v_draws.id AND v_wins.t_id = v_draws.t_id
+    ORDER BY t_id, score DESC;
 
 -- Byes View
 -- Create a view showing if a player has had a bye
 CREATE VIEW v_byes AS 
-    SELECT foo.t_id, foo.id,foo.name,count(bye) AS byes
+    SELECT COALESCE(foo.t_id, 0) AS t_id, foo.id,foo.name,count(bye) AS byes
     FROM (SELECT * FROM players LEFT JOIN registeredPlayers ON players.id = registeredPlayers.p_id) AS foo 
-    LEFT JOIN matches ON (winner=foo.id OR loser=foo.id) AND matches.bye = 't'
+    LEFT JOIN matches ON (winner=foo.id OR loser=foo.id) AND matches.bye = 't' AND matches.t_id = foo.t_id
     GROUP BY foo.t_id, foo.id, foo.name;
 
 -- Matches View
 -- Creates a view showing all matches played by each player
 CREATE VIEW v_matches AS 
-    SELECT matches.t_id,players.id,players.name,count(matches) AS matches 
-    FROM players LEFT JOIN matches ON winner=players.id OR loser=players.id GROUP BY matches.t_id, players.id, players.name;
+    SELECT COALESCE(foo.t_id, 0) AS t_id,foo.id,foo.name,count(matches) AS matches 
+    FROM (SELECT * FROM players LEFT JOIN registeredPlayers ON players.id = registeredPlayers.p_id) AS foo 
+    LEFT JOIN matches ON (winner=foo.id OR loser=foo.id) AND matches.t_id = foo.t_id
+    GROUP BY foo.t_id, foo.id, foo.name;
 
 -- Opponent Match Wins View
 -- Uses the v_scores view along with the matches table unioned with itself to create a omw view
@@ -98,7 +101,8 @@ FROM v_scores;
 -- Uses v_omw, v_matches, v_byes to create a view that lists matches won and played for each player
 CREATE VIEW v_standings AS 
     SELECT v_oms.t_id, v_oms.id, v_oms.name, v_oms.wins, v_oms.draws, v_matches.matches, v_oms.score, v_oms.oms, v_byes.byes
-    FROM v_oms,v_matches,v_byes WHERE v_oms.id = v_matches.id AND v_oms.id = v_byes.id ORDER BY t_id, wins DESC, oms DESC, id DESC;
+    FROM v_oms,v_matches,v_byes WHERE v_oms.id = v_matches.id AND v_oms.id = v_byes.id  AND v_oms.t_id = v_matches.t_id AND v_oms.t_id = v_byes.t_id
+    ORDER BY t_id, wins DESC, oms DESC, id DESC;
 
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ TESTING AREA ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
