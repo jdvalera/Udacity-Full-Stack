d813009d8746f76dae3e553c6048e5e0cb879eb0
John Valera <johnlemuelvalera@yahoo.com>
2015-12-01 00:24:17 -0800

    Update pg_config.sh

--- Project3/vagrant/pg_config.sh
+++ Project3/vagrant/pg_config.sh
@@ -14,8 +14,7 @@ pip install flask-login
 pip install flask-seasurf
 su postgres -c 'createuser -dRS vagrant'
 su vagrant -c 'createdb'
-su vagrant -c 'createdb forum'
-su vagrant -c 'psql forum -f /vagrant/forum/forum.sql'
+
 
 vagrantTip="[35m[1mThe shared directory is located at /vagrant\nTo access your shared files: cd /vagrant(B[m"
 echo -e $vagrantTip > /etc/motd
