ad4ec5e2f6abc9d22acf61552aaaff14271f1b5c
jdvalera <johnlemuelvalera@yahoo.com>
2015-11-30 18:32:18 -0800

    Code clean up

--- Project3/vagrant/catalog/database_setup.py
+++ Project3/vagrant/catalog/database_setup.py
@@ -4,11 +4,10 @@ from sqlalchemy import Column, ForeignKey, Integer, String, \
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
-from flask.ext.login import UserMixin
  
 Base = declarative_base()
 
-class User(Base, UserMixin):
+class User(Base):
   __tablename__ = 'user'
 
   id = Column(Integer, primary_key = True)
